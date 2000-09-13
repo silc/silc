b47b8d3caa25ea3b555617460a1f3b97f2cb2923
Pekka Riikonen <priikone@silcnet.org>
2000-09-13 17:56:35 +0000

    Created.

---
+++ includes/clientlibincludes.h
@@ -0,0 +1,37 @@
+/*
+
+  clientlibincludes.h
+
+  Author: Pekka Riikonen <priikone@poseidon.pspt.fi>
+
+  Copyright (C) 1997 - 2000 Pekka Riikonen
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+*/
+
+#ifndef CLIENTLIBINCLUDES_H
+#define CLIENTLIBINCLUDES_H
+
+#include "silcdefs.h"
+
+/* Generic includes */
+#include "silcincludes.h"
+
+/* SILC Client includes */
+#include "client.h"
+#include "command.h"
+#include "command_reply.h"
+#include "idlist.h"
+#include "protocol.h"
+#include "ops.h"
+
+#endif
