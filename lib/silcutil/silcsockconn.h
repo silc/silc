72e3a28c7398a56d25433bcbbd1e54576777f802
Pekka Riikonen <priikone@silcnet.org>
2001-08-12 11:10:42 +0000

    updates.

--- lib/silcutil/silcsockconn.h
+++ lib/silcutil/silcsockconn.h
@@ -1,24 +1,26 @@
+/*
+ 
+  silcsockconn.h
+ 
+  COPYRIGHT
+ 
+  Author: Pekka Riikonen <priikone@silnet.org>
+ 
+  Copyright (C) 1997 - 2001 Pekka Riikonen
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+*/
+
 /****h* silcutil/silcsockconn.h
- *
- * NAME
- *
- * silcsockconn.h
- *
- * COPYRIGHT
- *
- * Author: Pekka Riikonen <priikone@silnet.org>
- *
- * Copyright (C) 1997 - 2001 Pekka Riikonen
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  *
  * DESCRIPTION
  *
@@ -373,6 +375,7 @@ typedef void (*SilcSocketHostLookupCb)(SilcSocketConnection sock,
  * SYNOPSIS
  *
  *    void silc_socket_host_lookup(SilcSocketConnection sock,
+ *                                 bool port_lookup,
  *                                 SilcSocketHostLookupCb callback,
  *                                 void *context,
  *                                 SilcSchedule schedule);
