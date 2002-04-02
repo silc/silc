ed3183ba3f87285d6ce477df1df7533e49a84895
Giovanni Giacobbi <johnny@silcnet.org>
2002-04-02 00:40:41 +0000

    i prefer consistent copyright name.

--- apps/silcd/serverconfig.h
+++ apps/silcd/serverconfig.h
@@ -4,7 +4,7 @@
 
   Author: Johnny Mnemonic <johnny@themnemonic.org>
 
-  Copyright (C) 1997 - 2002 Johnny Mnemonic
+  Copyright (C) 1997 - 2002 Pekka Riikonen
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
