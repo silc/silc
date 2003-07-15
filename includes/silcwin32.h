aef7cb0ada75acbda1017306ad324835c85e28c6
Pekka Riikonen <priikone@silcnet.org>
2003-07-15 14:32:34 +0000

    updates.

--- includes/silcwin32.h
+++ includes/silcwin32.h
@@ -4,13 +4,13 @@
 
   Author: Pekka Riikonen <priikone@silcnet.org>
 
-  Copyright (C) 2001 Pekka Riikonen
+  Copyright (C) 2001, 2003 Pekka Riikonen
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -36,7 +36,11 @@
 
 #ifdef WIN32
 #ifndef DLL
+#ifndef _LIB
 #define DLLAPI __declspec(dllimport)
+#else
+#define DLLAPI // Nada, we use .DEF
+#endif
 #else
 #define DLLAPI // Nada, we use .DEF
 #endif
