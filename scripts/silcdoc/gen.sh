56fa26c39a086e22141a3ee76a5fdf8a9028b08d
Pekka Riikonen <priikone@silcnet.org>
2005-05-01 08:00:01 +0000

    updates.

--- scripts/silcdoc/gen.sh
+++ scripts/silcdoc/gen.sh
@@ -3,6 +3,6 @@
 cat << EOF > tmp.php
 <?php \$page="$4"; \$dest="$1"; \$type="$3"; require "$2"; ?>
 EOF
-php -f tmp.php >$5.tmp
-mv $5.tmp $5
+php -f tmp.php >"$5".tmp
+mv "$5".tmp "$5"
 rm -f tmp.php
